21470f8665ea665851ce421c04e46787d27a54fe
generateNscore <98548879+PVLaboratory@users.noreply.github.com>
2023-04-07 22:59:06 +0900

    Update generateSheets_Example1.py

--- Examples/generateSheets_Example1.py
+++ Examples/generateSheets_Example1.py
@@ -1,4 +1,4 @@
-import generateNscore as gNs
+import quizhtml
 
 clock='''var argsFromMain=null;function init(prms) {argsFromMain=prms;}//init();
 if (argsFromMain == null) {argsFromMain=[300, 10, 10];}const width= argsFromMain[0];cnvs.width=width;cnvs.height=width;ctx.clearRect(0,0, cnvs.width, cnvs.height);var radius = width/2;ctx.translate(radius, radius);radius = radius * 0.90;
@@ -253,4 +253,4 @@ STDs={'12345678': 'oooo'}
 iMultipleChoices=False
 
 a=gNs.work(name, heading, STDs, QGs, True, iMultipleChoices, False)
-gNs.mkHTMLs(a, figures)
+quizhtml.mkHTMLs(a, figures)
